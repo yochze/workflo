061afc3c2405440c43eb5cb70f7481660b829432
Yochay <yochze@gmail.com>
2013-03-19 12:01:20 +0200

    initializing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workflo::Application.config.secret_token = '283ccc136f5ec80f3e6f7a59269bb27144303cedf3b2c674be46a1013506394daf7d941a0021fcaa617f5658c279bf8625d3edc66b37238803efbe4ac4fb2b60'
